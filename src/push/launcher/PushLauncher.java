1d1801f85fe4fda18ff8d22edbed2ec2ed03bcbc
renxinwei <413038044@qq.com>
2019-04-11 11:31:47 +0800

    重构，脱离push服务器

---
+++ src/push/launcher/PushLauncher.java
@@ -0,0 +1,45 @@
+package push.launcher;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import cn.jpush.api.JPushClient;
+import cn.jpush.api.common.resp.APIConnectionException;
+import cn.jpush.api.common.resp.APIRequestException;
+import cn.jpush.api.push.model.Options;
+import cn.jpush.api.push.model.Platform;
+import cn.jpush.api.push.model.PushPayload;
+import cn.jpush.api.push.model.audience.Audience;
+import cn.jpush.api.push.model.notification.Notification;
+
+/**
+ * @ClassName: ApiDemo
+ * @Description: TODO
+ *
+ */
+public class PushLauncher {
+	private static Logger logger = Logger.getLogger(PushLauncher.class);
+
+	JPushClient jPushClient = null;
+
+	public void registerMeisheng(String masterSecret, String appKey) {
+		jPushClient = new JPushClient(masterSecret, appKey);
+	}
+
+	public void sendNotification(String[] alias, String mobile, String notification)
+			throws APIConnectionException, APIRequestException {
+		sendNotification(Arrays.asList(alias), mobile, notification);
+	}
+
+	public void sendNotification(List<String> alias, String mobile, String notification)
+			throws APIConnectionException, APIRequestException {
+		jPushClient.sendPush(PushPayload.newBuilder().setNotification(Notification.alert(notification))
+				.setPlatform(Platform.all()).setAudience(Audience.alias(alias))
+				.setOptions(Options.newBuilder().setApnsProduction(true).build()).build());
+	}
+
+}
